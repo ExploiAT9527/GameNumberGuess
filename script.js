72997b7d1406272593630ae032f9103d017dd1dd
ExpLoiAT <c3157550532@163.com>
2025-07-09 22:48:49 +0800

    Update script.js
    Improve the UX, Now for more convenience, pressing the Enter does the same as clicking the button .

--- script.js
+++ script.js
@@ -6,7 +6,12 @@ const lastResult = document.querySelector(".lastResult");
 const lowOrhight = document.querySelector(".lowOrhight")
 let guessCount = 1;
 let resetButton;
+let guessHistory = [];
+
 function checkGuess() {
+    if (userInput.value === "") {
+        document.querySelector("form").addEventListener("submit", function (e) { e.preventDefault(); });
+        }
     const userGuess = Number(userInput.value);
     if (userGuess === randomNumber) {
         lowOrhight.textContent = "Congratulations! You got it right!";
@@ -25,12 +30,15 @@ function checkGuess() {
         lowOrhight.textContent = "Last guess is too low";
     }
 
+    guessHistory.push(userInput.value);
+    lastResult.textContent = "History:"+guessHistory.join(",");
     guessCount++;
     userInput.value = "";
 }
 
 submitButton.addEventListener("click", checkGuess);
-
+document.removeEventListener("keydown", enterreset);
+document.addEventListener("keydown", entersubmit );
 function gameOver() {
     userInput.disabled = true;
     submitButton.disabled = true;
@@ -38,15 +46,33 @@ function gameOver() {
     resetButton.textContent = "Start new game";
     document.body.appendChild(resetButton);
     resetButton.addEventListener("click", resetGame);
+    document.removeEventListener("keydown",entersubmit);
+    document.addEventListener("keydown", enterreset);
 }
 
 function resetGame() {
     guessCount = 1;
+    guessHistory.length = 0;
+    lastResult.textContent = "History:" + guessHistory.join(",");
     lowOrhight.textContent = ""; 
     userInput.disabled = false;
     submitButton.disabled = false;
     resetButton.parentNode.removeChild(resetButton);
     userInput.value = "";
-    userInput.focus;
-    let randomNumber = Math.floor(Math.random() * 10 + 1);
+    userInput.focus();
+    document.removeEventListener("keydown", enterreset);
+    document.addEventListener("keydown", entersubmit);
+    randomNumber = Math.floor(Math.random() * 10 + 1);
+}
+function entersubmit(e) {
+    if (e.key === "Enter") {
+
+        checkGuess();
+    }
+}
+
+function enterreset(e) {
+    if (e.key === "Enter") {
+        resetGame();
+    }
 }
